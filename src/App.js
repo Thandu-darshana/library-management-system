02870ef6019bdc4e41c508d0a7a2cf7d21b630f7
RdhYuGaan <radithyugan22@gmail.com>
2024-09-28 20:00:08 +0530

    add login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import DashboardLayoutNavigationNested from './components/Dashboard';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './components/Login'; 
+
 
 function App() {
   return (
-    <div className="App">
-      <DashboardLayoutNavigationNested></DashboardLayoutNavigationNested>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} /> 
+        <Route path="/dashboard" element={<DashboardLayoutNavigationNested />} /> 
+      </Routes>
+    </Router>
   );
+    
+  
 }
 
 export default App;
